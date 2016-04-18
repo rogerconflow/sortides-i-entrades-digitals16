956a6d7dad9cc542de1fd3861f70b4b34c72b52e
rogerlconflow <rogerlladom@gmail.com>
2016-04-18 14:56:06 +0200

    FYJKXK

--- ex_1.1_digital/ex_1.1_digital.ino
+++ ex_1.1_digital/ex_1.1_digital.ino
@@ -7,7 +7,6 @@ int ledPin6= 7;
 int ledPin7= 6;
 int ledPin8= 5; 
 
-
 void setup()
 {
   pinMode(ledPin , OUTPUT );  //li diem que son sortides 
@@ -18,36 +17,28 @@ void setup()
   pinMode(ledPin6 , OUTPUT ); 
   pinMode(ledPin7 , OUTPUT ); 
   pinMode(ledPin8 , OUTPUT ); 
-  
-  
-  
- 
-
 }
 
 void loop()
 {
  
-   digitalWrite(ledPin , LOW );
+   digitalWrite(ledPin , HIGH );
    digitalWrite(ledPin2 , HIGH );
-   digitalWrite(ledPin3 , LOW );  //fem intermitent amb els leds 
+   digitalWrite(ledPin3 , HIGH );  //fem intermitent amb els leds 
    digitalWrite(ledPin4 , HIGH );
-   digitalWrite(ledPin5 , LOW );
+   digitalWrite(ledPin5 , HIGH );
    digitalWrite(ledPin6 , HIGH );
-   digitalWrite(ledPin7 , LOW );
+   digitalWrite(ledPin7 , HIGH );
    digitalWrite(ledPin8 , HIGH );
   delay(1000);
   
-   digitalWrite(ledPin , HIGH );
+   digitalWrite(ledPin , LOW );
    digitalWrite(ledPin2 , LOW );
-   digitalWrite(ledPin3 , HIGH );
+   digitalWrite(ledPin3 , LOW );
    digitalWrite(ledPin4 , LOW );
-   digitalWrite(ledPin5 , HIGH );
+   digitalWrite(ledPin5 , LOW );
    digitalWrite(ledPin6 , LOW );
-   digitalWrite(ledPin7 , HIGH );
+   digitalWrite(ledPin7 , LOW );
    digitalWrite(ledPin8 , LOW );
   delay(1000);
-    
- 
-
 }
